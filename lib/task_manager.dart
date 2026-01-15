0f1fb69b995ffa83384570cb48c419dcc6780315
Motaleb Hossain <motaleb.it@gmail.com>
2026-01-15 17:35:55 +0600

    firebase task manager app design running update

--- lib/task_manager.dart
+++ lib/task_manager.dart
@@ -2,6 +2,39 @@ import 'package:flutter/material.dart';
 class TaskManager extends StatelessWidget {
   const TaskManager({super.key});
 
+  void showTaskDialog(BuildContext context){
+    TextEditingController titleController = TextEditingController();
+    TextEditingController descriptionController = TextEditingController();
+    showDialog(context: context, builder: (_)=>AlertDialog(
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: 'Task title'
+              ),
+            ),
+            TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(
+                labelText: 'description'
+              ),
+            ),
+            
+          ],
+        ),
+      actions: [
+        TextButton(onPressed: (){
+          Navigator.pop(context);
+        }, child: Text('Cancel')),
+        ElevatedButton(onPressed: (){}, child: Text('Add'))
+      ],
+      
+    ));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,16 +47,18 @@ class TaskManager extends StatelessWidget {
         itemCount: 10,
           itemBuilder: (context, index){
             return ListTile(
-              
               title: Text('This is new task '),
               subtitle: Text('This is new task '),
+              leading: Checkbox(value: false, onChanged: (val){}),
               trailing: Icon(Icons.edit),
             );
           }
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.teal.shade100,
-        onPressed: (){}, child: Icon(Icons.add),
+        onPressed: (){
+          showTaskDialog(context);
+        }, child: Icon(Icons.add),
       ),
     );
   }

f8b18bb9a574cfa1d73b32fc274912700dbe280d
Motaleb Hossain <motaleb.it@gmail.com>
2026-01-17 17:37:21 +0600

    complete task project

--- lib/task_manager.dart
+++ lib/task_manager.dart
@@ -1,11 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 class TaskManager extends StatelessWidget {
-  const TaskManager({super.key});
+   TaskManager({super.key});
+   
+   final CollectionReference tasks = FirebaseFirestore.instance.collection('tasks');
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
 
-  void showTaskDialog(BuildContext context){
-    TextEditingController titleController = TextEditingController();
-    TextEditingController descriptionController = TextEditingController();
+  Future<void> updateTasks(String id, bool completed) async {
+   await tasks.doc(id).update({
+      'title':titleController.text,
+      'description':descriptionController.text,
+      'completed': completed,
+    });
+  }
+
+   Future<void> updateStatusTasks(String id, bool completed) async {
+     await tasks.doc(id).update({
+       'completed': completed,
+     });
+   }
+
+  Future<void> deleteTasks(String id) async {
+    await tasks.doc(id).delete();
+  }
+
+  Future<void> addTasks() async{
+    await tasks.add({
+      'title':titleController.text,
+      'description':descriptionController.text,
+      'completed': false,
+    });
+    titleController.clear();
+    descriptionController.clear();
+  }
+
+  void showTaskDialog(BuildContext context, [DocumentSnapshot ? doc]){
+
+    //doc !=null 'Update Task': 'Add Task';
+    if(doc != null){
+      titleController.text = doc['title'];
+      descriptionController.text = doc['description'];
+    }else{
+      titleController.clear();
+      descriptionController.clear();
+    }
+
+
     showDialog(context: context, builder: (_)=>AlertDialog(
+      title: Text(doc !=null ? 'Update task' : 'Add task'),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -28,7 +72,10 @@ class TaskManager extends StatelessWidget {
         TextButton(onPressed: (){
           Navigator.pop(context);
         }, child: Text('Cancel')),
-        ElevatedButton(onPressed: (){}, child: Text('Add'))
+        ElevatedButton(onPressed: (){
+         doc != null ? updateTasks(doc.id, doc['completed']): addTasks();
+          Navigator.pop(context);
+        }, child: Text(doc != null ? 'Update' : 'Add'))
       ],
       
     ));
@@ -43,16 +90,44 @@ class TaskManager extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.teal,
       ),
-      body: ListView.builder(
-        itemCount: 10,
-          itemBuilder: (context, index){
-            return ListTile(
-              title: Text('This is new task '),
-              subtitle: Text('This is new task '),
-              leading: Checkbox(value: false, onChanged: (val){}),
-              trailing: Icon(Icons.edit),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: tasks.snapshots(),
+          builder: (context, asyncSnapshot) {
+            if(!asyncSnapshot.hasData) return Center(child: CircularProgressIndicator(),);
+            final docs = asyncSnapshot.data!.docs;
+            return ListView.builder(
+              itemCount: docs.length,
+                itemBuilder: (context, index){
+                final doc = docs[index];
+                  return Slidable(
+                    key: ValueKey(doc.id),
+                    endActionPane: ActionPane(motion: DrawerMotion(), children: [
+                      SlidableAction(onPressed: (_)=>deleteTasks(doc.id),
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Delete',
+                      ),
+                    ]),
+                    child: Card(
+                      child: ListTile(
+                        title: Text(doc['title']),
+                        subtitle: Text(doc['description']),
+                        leading: Checkbox(value: doc['completed'], onChanged: (val){
+                          updateStatusTasks(doc.id, val!);
+                        }),
+                        trailing: IconButton(onPressed: (){
+                          showTaskDialog(context, doc);
+                        }, icon: Icon(Icons.edit)),
+                      ),
+                    ),
+                  );
+                }
             );
           }
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.teal.shade100,
